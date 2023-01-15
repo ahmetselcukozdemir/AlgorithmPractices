cea645669258d6259dfddbdc8ce4525097884df0
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2023-01-15 16:23:54 +0300

    15_01_2023

---
+++ AlgorithmsPractice/AlgorithmsPratice.Class/PerfectValidSquare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AlgorithmsPratice.Class
+{
+    public class PerfectValidSquare
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            long a = 1;
+            long b = num;
+            while (a <= b)
+            {
+                long middle = (b - a) / 2 + a;
+
+                if (middle * middle == num) return true;
+                if (middle * middle > num) b = middle - 1;
+                else a = middle + 1;
+            }
+
+            return false;
+        }
+    }
+}
